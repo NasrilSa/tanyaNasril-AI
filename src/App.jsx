942a714f7b76d33764b7d214519b0a532cd4e363
NasrilSa <nasrililhamsa@gmail.com>
2024-05-31 21:24:21 +0700

    change design

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { requestToGroqAI } from "./utils/groq";
 import { Light as SyntaxHighlight } from "react-syntax-highlighter";
 import { darcula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
 function App() {
   const [data, setData] = useState("");
   const handleSubmit = async () => {
@@ -11,22 +12,25 @@ function App() {
   };
   return (
     <main className="flex flex-col min-h-[80vh] justify-center items-center max-w-xl w-full mx-auto">
-      <h1 className="text-4xl text-indigo-500 ">tanyaNasril-AI</h1>
-      <form className="flex flex-col gap-4 py-4 w-full">
+      <h1 className="text-3xl text-green-400 font-extrabold mb-2 flex">
+        tanyaNasril-AI
+      </h1>
+      <form className="flex  gap-4 py-2 w-full rounded-[50px] px-2 bg-slate-800 text-white transition duration-300 ease-in-out transform hover:shadow-lg hover:shadow-green-500">
         <input
           type="text"
           placeholder="ketik permintaan disini"
-          className="py-2 px-4 text-md rounded-md"
+          className="py-2 px-4 text-md w-full border-none outline-none bg-slate-800"
           id="content"
         />
         <button
           type="button"
           onClick={handleSubmit}
-          className="bg-indigo-500 py-2 px-4 font-bold text-white rounded-md"
+          className="bg-green-700 py-2 px-4 font-bold text-white rounded-[50px] hover:bg-green-500"
         >
-          Kirim
+          ⭡
         </button>
       </form>
+
       <div className="max-w-xl w-full mx-auto">
         {data ? (
           <SyntaxHighlight
